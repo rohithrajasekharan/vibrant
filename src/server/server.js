b6011b9b8b2fd83a4e9f88da29bd930fba393889
rohithrajasekharan <rohithrajasekharan@outlook.com>
2021-04-28 18:09:32 +0530

    html reduction

--- src/server/server.js
+++ src/server/server.js
@@ -25,43 +25,21 @@ server
         <App />
       </StaticRouter>
     );
-    const html = `<!doctype html>
-<html lang="">
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta charset="utf-8" />
-    <title>Welcome to Razzle</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    ${assets.client.css
-        ? `<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link rel="stylesheet" href="${assets.client.css}">`
-        : ''
-      }
-    ${process.env.NODE_ENV === 'production'
-        ? `<script src="${assets.client.js}" defer></script>`
-        : `<script src="${assets.client.js}" defer crossorigin></script>`
-      }
-</head>
-<body>
-    <div id="root">${markup}</div>
-</body>
-</html>`
 
     const helmet = Helmet.renderStatic();
     if (context.url) {
       res.redirect(context.url);
     } else {
-      res.status(200).send(formatHTML(html, helmet));
+      res.status(200).send(formatHTML(helmet));
     }
 
 
-    function formatHTML(appStr, helmet) {
+    function formatHTML(helmet) {
       return `
         <!DOCTYPE html>
         <html lang="en">
           <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
           <meta http-equiv="cache-control" content="no-cache" />
             ${helmet.title.toString()}
             ${helmet.meta.toString()}
